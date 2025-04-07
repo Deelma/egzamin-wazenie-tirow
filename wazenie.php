5270ba9de4e21731a44d03d2ab2c83648f90c071
Deelma <156914350+Deelma@users.noreply.github.com>
2025-04-07 11:24:40 +0200

    Add files via upload

---
+++ wazenie.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ważenie samochodów ciężarowych</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <section id="goralewo">
+            <h1>Ważenie pojazdów we Wrocławiu</h1>
+        </section>
+        <section id="goraprawo">
+            <img src="obraz1.png" alt="waga">
+        </section>
+    </header>
+    <main>
+        <section id="lewy">
+            <h2>Lokalizacje wag</h2>
+            <ol>
+                <!-- ! skrypt -->
+            </ol>
+            <h2>Kontakt</h2>
+            <a href="mailto:wazenie@wroclaw.pl"></a>
+        </section>
+        <section id="srodkowy">
+            <h2>Alerty</h2>
+            <table>
+                <tr><th>rejestracja</th><th>ulica</th><th>waga</th><th>dzień</th><th>czas</th></tr>
+            </table>
+        </section>
+        <section id="prawy">
+
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
